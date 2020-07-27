658444c20498a990b00c145d36231fc231f61520
jgrt <asati.jagrati93@gmail.com>
2020-07-28 00:14:49 +0530

    Unit tests for solutions 1 to 5.

---
+++ programs/mathematics/problem1/test_solution.py
@@ -0,0 +1,23 @@
+import pytest
+from problem1.solution import create_pattern
+
+
+def test_create_pattern_on_output_size():
+    val = 4
+    resp = create_pattern(val)
+    assert len(resp) == val
+
+
+def test_create_pattern_on_order_of_list_element():
+    resp = create_pattern(3)
+    assert list(resp[-1]) == sorted(list(resp[-1]), reverse=True)
+
+
+def test_create_pattern_on_size_elements():
+    val = 3
+    resp = create_pattern(val)
+    elements_len = list(map(len, resp))
+    assert elements_len == [val*i for i in range(val, 0, -1)]
+
+
+
---
+++ programs/mathematics/problem5/test_solution.py
@@ -0,0 +1,3 @@
+from problem5.solution import find_kth_digit
+
+# def test_find_kth_digit_on_
