58deba3db4d7623f30534b6824e94a74e4b24975
jgrt <asati.jagrati93@gmail.com>
2020-08-03 17:45:06 +0530

    Solution - Help Nobita (odd or even problem).

--- programs/bit-magic/problem2/solution.py
+++ programs/bit-magic/problem2/solution.py
@@ -13,3 +13,40 @@ Constraints:
 Input: S = abbbcc
 Output: ODD
 """
+from pydantic import BaseModel, Field
+
+
+is_even = lambda x: 1 if x % 2 == 0 else 0
+is_odd = lambda x: 1 if x % 2 != 0 else 0
+
+
+class ConstrainedString(BaseModel):
+    value: str = Field(..., min_length=1, max_length=pow(10, 3), regex='[a-z]')
+
+
+def even_or_odd(value: str) -> str:
+    letters = list('abcdefghijklmnopqrstuvwxyz')
+    letter_count = dict()
+    for i in value:
+        if i in letter_count:
+            count = letter_count[i]
+            letter_count[i] = count + 1
+        else:
+            letter_count[i] = 1
+    x = y = 0
+    for k, v in letter_count.items():
+        letter_index = letters.index(k) + 1
+        letter_occurrences = v
+        if is_even(letter_index) and is_even(letter_occurrences):
+            x += 1
+        if is_odd(letter_index) and is_odd(letter_occurrences):
+            y += 1
+    if is_even(x + y):
+        return 'EVEN'
+    return 'ODD'
+
+
+if __name__ == '__main__':
+    val = 'nobitaa'
+    value = ConstrainedString(value=val).value
+    print(even_or_odd(value))
