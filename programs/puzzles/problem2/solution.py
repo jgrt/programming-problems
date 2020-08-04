dc71994f385a3dc785711d88eabbbb90701d66b9
jgrt <asati.jagrati93@gmail.com>
2020-08-04 21:29:10 +0530

    Solution - 3 divisors

--- programs/puzzles/problem2/solution.py
+++ programs/puzzles/problem2/solution.py
@@ -10,3 +10,26 @@ Constraints :
 Input: N = 6
 Output: 1
 """
+from pydantic import BaseModel, Field
+from math import sqrt
+
+
+class ConstrainedInteger(BaseModel):
+    value: int = Field(..., ge=1, le=pow(10, 12))
+
+
+def three_divisors(n: int) -> int:
+    l = int(sqrt(n+1))+1
+    a = [True]*l
+    for i in range(2, l):
+        if a[i]:
+            for j in range(i*2, l, i+2):
+                a[j] = False
+    a = [i**2 for i, ele in enumerate(a) if i > 1 and ele and i**2 <= n]
+    return len(a)
+
+
+if __name__ == '__main__':
+    n = pow(10, 10)
+    n = ConstrainedInteger(value=n).value
+    print(three_divisors(n))
