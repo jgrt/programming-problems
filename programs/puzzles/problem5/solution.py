8ce32e2775875e77c4092eb113c11a5dc4554fc0
jgrt <asati.jagrati93@gmail.com>
2020-08-05 01:11:52 +0530

    Solution - angle between hour and minute hand.

--- programs/puzzles/problem5/solution.py
+++ programs/puzzles/problem5/solution.py
@@ -12,3 +12,27 @@ Constraints:
 Input: h = 3, m = 30
 Output: 75
 """
+from pydantic import BaseModel, Field
+from math import floor
+
+
+class ConstrainedIntegers(BaseModel):
+    hour: int = Field(..., ge=1, le=12)
+    minute: float = Field(..., ge=1, le=60)
+
+
+def angle_bw_hour_minute_hand(h: int, m: float) -> int:
+    if m == 60:
+        h -= 1
+    hour_hand = h * 30 + m * .5
+    minute_hand = m * 6
+    angle1 = abs(hour_hand - minute_hand)
+    angle2 = 360 - angle1
+    return floor(min(angle1, angle2))
+
+
+if __name__ == '__main__':
+    hour = 6
+    minute = 33.8151
+    constrained_values = ConstrainedIntegers(hour=hour, minute=minute)
+    print(angle_bw_hour_minute_hand(constrained_values.hour, constrained_values.minute))
