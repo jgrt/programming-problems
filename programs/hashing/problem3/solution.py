430d5fee475c2950ebac1843eafb5d00c1cbedbc
jgrt <asati.jagrati93@gmail.com>
2020-08-06 19:09:35 +0530

    Solution - Frequency of array elements from 1 to n.

--- programs/hashing/problem3/solution.py
+++ programs/hashing/problem3/solution.py
@@ -12,3 +12,26 @@ Constraints:
 Input: N = 5, arr = 2 3 2 3 5
 Output: 0 2 2 0 1
 """
+from pydantic import BaseModel, Field, validator
+from typing import List
+from collections import Counter
+
+
+class ConstrainedArray(BaseModel):
+    array: List[int] = Field(..., min_items=1, max_items=pow(10, 6))
+
+    @validator("*", pre=True, each_item=True)
+    def elements_must_be_in_range(cls, v):
+        assert 1 <= v <= pow(10, 6), 'must be within range {}'.format([0, pow(10, 6)])
+        return v
+
+
+def elements_freq(arr: List[int]) -> List[int]:
+    freq_dict = Counter(arr)
+    return [freq_dict[i + 1] if i + 1 in freq_dict else 0 for i in range(len(arr))]
+
+
+if __name__ == '__main__':
+    array = [1, 2, 5, 5, 3]
+    array = ConstrainedArray(array=array).array
+    print(elements_freq(array))
